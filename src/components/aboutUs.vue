3d6e33af52831e0348022854b06cb40fa106f2c4
Tarek Hallak <HallakTarek7@gmail.com>
2025-04-02 13:58:06 +0300

    edit the sizes

--- src/components/aboutUs.vue
+++ src/components/aboutUs.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class= "p-8 ">
+  <div class= "p-16 ">
     <div class="text-left">
-      <h2 class="text-2xl font-bold mb-4 mt-4">About us</h2>
-      <h1 class="text-4xl font-bold mb-4 ">Set your goals high, and don't stop till you get there.</h1>
-      <p class="text-gray-600 text-lg">
+      <h2 class="text-3xl font-bold mb-4 mt-4">About us</h2>
+      <h1 class="text-5xl font-bold mb-4 ">Set your goals high, and don't stop till you get there.</h1>
+      <p class="text-gray-600 text-xl">
         Nulla varius consequat magna, id molestie ipsum volutpat quis. A true story, that never been told! Fusce id mi
         diam, non ornare. Fusce id mi diam, non ornare orci. Pellentesque ipsum erat, facilisis ut venenatis eu.
       </p>
