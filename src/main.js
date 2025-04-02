dd7524e111407e92530520548cfed9d71ceded9a
Tarek Hallak <HallakTarek7@gmail.com>
2025-04-02 08:58:13 +0300

    tailwind fils

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+Alpine.start();
+
 const app = createApp(App)
 
 app.use(createPinia())
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+// tailwind.config.js
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    screens: {
+      'sm': '440px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
